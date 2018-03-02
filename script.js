c02910429f46c1617cc6381d34ee8f63c4052904
rambabugovind <rambabugovind@hotmail.com>
2018-03-01 20:14:04 -0600

    Stage-4

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 
 
-$(document).ready(function(){
-  $("#btn1").on({
-    click:function(){
-      $("#profile").text("");
-    }
-  });
-});
+var btn1 = document.querySelector('#btn1');
+var btn2 = document.querySelector('#btn2');
+
+btn1.addEventListener('click', insertContact);
+btn2.addEventListener('click', insertCards);
+
+const profile = document.getElementById('profile');
+const contact1 = document.getElementById('contact1');
+
+profile.style.display = "none";
+contact1.style.display = "none";
+
+function insertContact() { //add '\' to escape new line
+  profile.style.display = "none";
+  contact1.style.display = "";
+}
+
+function insertCards() {
+  contact1.style.display = "none";
+  profile.style.display = "";
+}
