2ad6d2bc0e499a90e8dae1401640b1370562ee91
rambabugovind <rambabugovind@hotmail.com>
2018-03-10 20:38:29 -0600

    Stage-6

--- script.js
+++ script.js
@@ -7,20 +7,20 @@ btn2.addEventListener('click', insertCards);
 const profile = document.getElementById('profile');
 const contact1 = document.getElementById('contact1');
 
-profile.style.display = "none";
-contact1.style.display = "none";
+// profile.style.display = "none";
+// contact1.style.display = "none";
 
 function insertContact() {
-  profile.style.display = "none";
-  contact1.style.display = ""; //to show div. u can use "block" but it spoils responsiveness
+  // profile.style.display = "none";
+  // contact1.style.display = "";
   contact1.scrollIntoView ({
     behavior: 'smooth'
   });
 }
 
 function insertCards() {
-  contact1.style.display = "none";
-  profile.style.display = "";
+  // contact1.style.display = "none";
+  // profile.style.display = "";
   profile.scrollIntoView ({
     behavior: 'smooth'
   });
